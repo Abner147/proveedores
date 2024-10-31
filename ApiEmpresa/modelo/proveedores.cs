013f1aba5653a8b7e6366dceb3e4c6ac150835ef
Abner147 <141387930+Abner147@users.noreply.github.com>
2024-10-30 22:14:40 -0600

    Add files via upload

---
+++ ApiEmpresa/modelo/proveedores.cs
@@ -0,0 +1,9 @@
+public class Proveedor
+{
+    public int IdProveedor { get; set; }
+    public string Nit { get; set; }
+    public string Proveedor { get; set; }
+    public string Direccion { get; set; }
+    public string RazonSocial { get; set; }
+    public DateTime FechaCreacion { get; set; }
+}
